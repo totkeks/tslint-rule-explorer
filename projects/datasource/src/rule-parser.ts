30268f0d196e7f609bad31471d893bde5e904a9e
Norman Dankert <norman.dankert@outlook.com>
2019-01-03 22:16:26 +0100

    add some more rule providers

--- projects/datasource/src/rule-parser.ts
+++ projects/datasource/src/rule-parser.ts
@@ -2,24 +2,38 @@ import * as glob from "glob";
 import * as fs from "fs";
 import { RuleMetadata } from "./rule-metadata";
 
-const ruleProviders = ["tslint", "codelyzer"];
+const ruleProviders = [
+	"tslint",
+	"codelyzer",
+	"rxjs-tslint-rules",
+	"tslint-eslint-rules",
+	"tslint-microsoft-contrib",
+	"tslint-consistent-codestyle",
+	"tslint-immutable",
+	"tslint-sonarts",
+];
 const rules: Array<RuleMetadata> = [];
 
 ruleProviders.forEach(provider =>
 	glob
 		.sync(`${provider}/**/*Rule.js`, { cwd: "node_modules" })
-		.forEach(matches => parseRule(matches, provider))
+		.forEach(matches => parseRule(matches))
 );
 
-function parseRule(match: string, provider: string) {
+function parseRule(match: string) {
 	const rule = require(match).Rule;
 
 	if (rule === undefined) {
 		return;
 	}
-
 	const metadata = rule.metadata as RuleMetadata;
-	metadata.provider = provider;
+
+	if (metadata === undefined) {
+		// TODO: infer partial metadata from filename
+		console.warn(`no metadata for rule in ${match}`);
+		return;
+	}
+	metadata.provider = match.split("/")[0];
 
 	rules.push(metadata);
 }
